de8e45bab60201cf9a8602bf44c2a33fadd48c2d
Ella Guest <ellamguest@gmail.com>
2017-10-11 18:52:18 +0100

    merging bq large files

---
+++ combing-large-files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 17:47:48 2017
+
+@author: emg
+"""
+
+import pandas as pd
+import glob 
+
+dtypes = {
+        'archived': 'str',
+        'author_flair_text': 'str',
+        'name': 'str',
+        'score_hidden': 'str'
+        }
+
+path = '/Users/emg/Programming/GitHub/aws/raw_data'
+files = glob.glob(path + "/*")
+dfs = []
+for f in files:
+    print(f)
+    df = pd.read_csv(f,index_col=None, header=0, dtype=dtypes)
+    dfs.append(df)
+    
+all_comments = pd.concat(dfs)
+
+all_comments.to_csv('/Users/emg/Programming/GitHub/aws/s3/td-all-comments.csv')
